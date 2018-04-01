346e83dbbe2dcd894de1dc9dc93de9515b15bcbd
Crawford Tech <crawfordtech@MacBook-Pro-4.local>
2018-04-01 13:07:40 -0400

    First draft of Valhalla script -- list instances

---
+++ Valhalla/valhalla.py
@@ -0,0 +1,21 @@
+import boto3
+
+
+
+if __name__ == '__main__':
+
+
+
+    session = boto3.Session(profile_name='Valhalla')
+
+
+
+    ec2 = session.resource('ec2')
+
+
+
+    for i in ec2.instances.all():
+
+        print (i)
+
+        

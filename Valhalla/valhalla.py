31efbab55a7c18f51b73e44df45270936eaa7e74
FreeTheBuddha <freethebuddha@Buddhas-production-mini.local>
2018-04-03 13:17:23 -0400

    Updated script to use the click module in order to show more information regarding the instances

--- Valhalla/valhalla.py
+++ Valhalla/valhalla.py
@@ -1,21 +1,21 @@
 import boto3
+import click   #this installs the click module
 
+session = boto3.Session(profile_name='Valhalla')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instance():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print (','.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
 
 
 if __name__ == '__main__':
-
-
-
-    session = boto3.Session(profile_name='Valhalla')
-
-
-
-    ec2 = session.resource('ec2')
-
-
-
-    for i in ec2.instances.all():
-
-        print (i)
-
-        
+    list_instance()

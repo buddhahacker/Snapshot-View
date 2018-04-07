7074d3bd70eef5343465df6805153d967e303e37
Buddhacker <buddhahacker@outlook.com>
2018-04-07 14:28:01 -0400

    Handled an AWS error condition when starting or stoping an instance in a pending state

--- Valhalla/valhalla.py
+++ Valhalla/valhalla.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click   #this installs the click module
 
 session = boto3.Session(profile_name='Valhalla')
@@ -144,7 +145,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print (" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -158,7 +163,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print (" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
